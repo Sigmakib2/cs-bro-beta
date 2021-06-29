93e1a287eb449463b077870b19a533cd0a8ea533
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2021-06-29 11:26:13 +0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBvxZMNGQDqh7q7veDjkrmwa-bZpk3OXcI",
+    authDomain: "techkib-counts.firebaseapp.com",
+    databaseURL: "https://techkib-counts-default-rtdb.firebaseio.com/",
+    storageBucket: "techkib-counts.appspot.com",
+    messagingSenderId: "809909373548"
+};
+firebase.initializeApp(config);
