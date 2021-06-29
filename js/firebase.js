e192f8ca941bfddf4cf5c9778900513c668dd645
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2021-06-29 11:09:06 +0600

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDReuaIy8n8oXtSSjglz3eIjTo4k0FxUJM",
+    authDomain: "demolikebutton.firebaseapp.com",
+    projectId: "demolikebutton",
+    storageBucket: "demolikebutton.appspot.com",
+    databaseURL: "https://demolikebutton-default-rtdb.firebaseio.com/",
+    messagingSenderId: "445881254839",
+    appId: "1:445881254839:web:f33afdc98ad7a6195c1e32"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
